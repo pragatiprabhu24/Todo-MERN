3448546700d0607d6a5dce0109be28540abdf600
pragatiprabhu24 <prabhupragati5@gmail.com>
2024-01-06 13:42:03 +0530

    added login endpoint

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,21 @@ app.post("/register", (req, res) => {
     .catch((err) => res.json(err));
 });
 
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  RegisterModel.findOne({ email: email }).then((user) => {
+    if (user) {
+      if ((user.password = password)) {
+        res.json("Success");
+      } else {
+        res.json("Wrong Password");
+      }
+    } else {
+      res.json("User not found");
+    }
+  });
+});
+
 app.listen(3001, (req, res) => {
   console.log("Server is running on port 3001");
 });
